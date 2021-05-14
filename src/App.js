ca18531937802f36c370a3e1267a142e960a144d
Lior Ben-David <liorbd@Outlook.com>
2021-05-14 00:51:16 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import { ResponsiveLine } from '@nivo/line'
+import {useState, useEffect} from 'react';
 
 function App() {
+  const url = "https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd&include_last_updated_at=true"
+    const [data, setData] = useState([
+     {
+       "id": "dogecoin",
+       "data": []
+     }
+  ]);
+  	const [price, setPrice] = useState(0)
+    const [lastFetched, setFetched] = useState("")
+  useEffect(() => {
+  
+     const intrvl = setInterval(() => {
+      let response = fetch(url).then(response => response.json()).then(resData => {
+        	 let dt = data
+             let d = new Date(0)
+             d.setUTCSeconds(resData.dogecoin.last_updated_at)
+          	 let cleanDate = d.toLocaleTimeString()
+          
+        
+          if(cleanDate != lastFetched) {
+          	console.log("New Price Update")
+            dt[0].data.push({x: cleanDate, y: resData.dogecoin.usd})
+            setData(dt)
+            
+            setFetched(cleanDate)
+            setPrice(resData.dogecoin.usd)
+          } else {
+          	console.log("Same Price")
+          }
+        
+          
+      })
+     }, 5000)
+     
+     return () => {
+     	clearInterval(intrvl)
+     }
+     
+  }, [data, price, lastFetched])
+  
+
+ 
+  const Graph = () => {
+  	return <div className="GraphContainer">
+          <ResponsiveLine
+          data={data}
+          margin={{ top: 50, right: 110, bottom: 50, left: 110 }}
+          xScale={{ type: 'point'}}
+          yScale={{ type: 'linear', min: 'auto' }}
+          axisBottom={{
+              orient: 'bottom',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'Time',
+              legendOffset: 36,
+              legendPosition: 'middle'
+          }}
+          axisLeft={{
+              orient: 'left',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'Price ($)',
+              legendOffset: -80,
+              legendPosition: 'middle'
+          }}
+          colors={{scheme: 'category10'}}
+          pointSize={5}
+          pointColor={{ theme: 'background' }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: 'serieColor' }}
+          pointLabelYOffset={-12}
+          useMesh={true}
+    	/>
+        </div>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<h1>Price: {price}</h1>
+      	<h3>Last Fetched: {lastFetched}</h3>
+      	<Graph/>
     </div>
   );
 }
